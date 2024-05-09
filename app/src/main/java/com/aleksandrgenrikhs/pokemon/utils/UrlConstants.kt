33f7c6b4e1a78278b634970d63606856ff8ed5e1
aleksandrgenrikhs <aleksandr.genrikhs@gmail.com>
2024-05-09 07:51:16 +0300

    fix

---
+++ app/src/main/java/com/aleksandrgenrikhs/pokemon/utils/UrlConstants.kt
@@ -0,0 +1,11 @@
+package com.aleksandrgenrikhs.pokemon.utils
+
+object UrlConstants {
+     const val BASE_URL = "https://pokeapi.co/api/v2/"
+    const val MAIN_IMAGE_URL =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/"
+    const val SHOW_DOWN_BACK_IMAGE_URL =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/back/"
+    const val SHOW_DOWN_FRONT_IMAGE_URL =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/"
+}
